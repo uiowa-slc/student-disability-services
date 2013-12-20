5bdbda340d7ba6eab3c919dabece5c445e5bbf81
Dustin Quam <quamsta@gmail.com>
2013-12-20 14:04:59 -0600

    initial commit

---
+++ mysite/_config.php
@@ -0,0 +1,27 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'omega',
+	"database" => 'sds',
+	"path" => '',
+);
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('simple');
+
+// Set the site locale
+i18n::set_locale('en_US');
+FulltextSearchable::enable();
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+Director::set_environment_type("dev");
