5e24badb37315e25cc713c7da2a1c03d9aa55676
Dustin Quam <quamsta@gmail.com>
2018-01-22 10:00:06 -0600

    disable default auth

--- mysite/_config.php
+++ mysite/_config.php
@@ -26,4 +26,5 @@ if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 if(Director::isLive()) {
 	Director::forceSSL();
 }
+Authenticator::unregister('MemberAuthenticator');
 Authenticator::set_default_authenticator('SAMLAuthenticator');
