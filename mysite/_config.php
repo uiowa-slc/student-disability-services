aa1d1f76be0cedfdecd0ef7f5f64d15a6477d006
Jessica Brown <jessica-e-brown@uiowa.edu>
2018-03-07 08:49:44 -0600

    upgrade to division 2.0

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,26 +4,23 @@ global $project;
 $project = 'mysite';
 
 global $database;
-
+//!!!!!!!Define your database name:
 $database = 'sds';
+ 
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
 
 MySQLDatabase::set_connection_charset('utf8');
-
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
 
 // Set the site locale
 i18n::set_locale('en_US');
 FulltextSearchable::enable();
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-//Director::set_environment_type("live");
-
 
 if(Director::isLive()) {
 	Director::forceSSL();
 }
 Authenticator::set_default_authenticator('SAMLAuthenticator');
+
+SSViewer::set_theme('division-subtheme');
